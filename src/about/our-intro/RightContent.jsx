25e2bb3dc3ecd6b86a1810f2b7b4f9613a214568
DragnelProjects <291502intekhab@gmail.com>
2026-03-30 12:44:23 +0530

    started the implementation of right section of top content of intro section

--- src/about/our-intro/RightContent.jsx
+++ src/about/our-intro/RightContent.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function RightContent() {
   return (
-    <div className=' w-[50%] flex_col items-center border '>RightContent</div>
+    <div className=' w-[50%] just_col items-center border '>
+      <img src="" className='h-[80%] w-[80%] rounded-xl ' />
+    </div>
   )
 }
 
