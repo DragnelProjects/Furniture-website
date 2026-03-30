0303bf494e043dfb7d10f3c7eb0333f04dfdc0c6
DragnelProjects <291502intekhab@gmail.com>
2026-03-30 06:53:14 +0530

    created the folders for different section of about page

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Section1 from '../section-one/Section1'
+import OurIntro from './our-intro/OurIntro'
+import Choose from './choose-us/Choose'
+import Curating from './curating-files/Curating'
+import OurTeam from './our-team/OurTeam'
 
 function About() {
   return (
     <div>
       <Section1/>
+      <OurIntro/>
+      <Choose/>
+      <Curating/>
+      <OurTeam/>
     </div>
   )
 }
