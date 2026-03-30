16b8cb0b79d1915fed765df8847eeffa0e99687a
DragnelProjects <291502intekhab@gmail.com>
2026-03-30 07:10:35 +0530

    created the files needed for the inrtyo section

---
+++ src/about/our-intro/BottomIntro.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BottomIntro() {
+  return (
+    <div>BottomIntro</div>
+  )
+}
+
+export default BottomIntro
---
+++ src/about/our-intro/TopIntro.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TopIntro() {
+  return (
+    <div>TopIntro</div>
+  )
+}
+
+export default TopIntro
