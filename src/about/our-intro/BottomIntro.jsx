b5d8223c455d95b05e3b344958fabecb83bb1247
DragnelProjects <291502intekhab@gmail.com>
2026-03-30 13:24:55 +0530

    implemation of inrto section is in progress

--- src/about/our-intro/BottomIntro.jsx
+++ src/about/our-intro/BottomIntro.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import {RiHeartFill, RiEqualizer2Fill, RiCustomerService2Fill} from '@remixicon/react'
 
 function BottomIntro() {
+
+  const story = [
+    {
+      logo:<RiHeartFill/>,
+      title: 'Founded With Passion',
+      discription: 'Started in 2015, FurniNest was born a passion for beautiful and functional living spaces'
+    },
+    {
+      logo:<RiEqualizer2Fill/>,
+      title: 'Founded With Passion',
+      discription: 'Started in 2015, FurniNest was born a passion for beautiful and functional living spaces'
+    }
+    ,{
+      logo:<RiCustomerService2Fill/>,
+      title: 'Founded With Passion',
+      discription: 'Started in 2015, FurniNest was born a passion for beautiful and functional living spaces'
+    }
+  ]
+
   return (
     <div  className='min-h-[50vh] just_col border'>
       <h2 className='title_text'>Our story</h2>
+      <div className='flex'>
+        {story.map((elem, idx) => (
+          <div key={idx} className='bg-amber-100'>
+            <div>{elem.logo}</div>
+            <h2>{elem.title}</h2>
+            <p>{elem.discription}</p>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
