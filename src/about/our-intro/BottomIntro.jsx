7e33479eaf0a4856cccd986edb70d311e620b829
DragnelProjects <291502intekhab@gmail.com>
2026-03-31 09:22:34 +0530

    intro section of about us page is completed

--- src/about/our-intro/BottomIntro.jsx
+++ src/about/our-intro/BottomIntro.jsx
@@ -11,25 +11,25 @@ function BottomIntro() {
     },
     {
       logo:<RiEqualizer2Fill/>,
-      title: 'Founded With Passion',
-      discription: 'Started in 2015, FurniNest was born a passion for beautiful and functional living spaces'
+      title: 'Growing With Quality',
+      discription: 'Our journey has been driven by redi-dication to quality, with a best on craftsmanship and timehee design.'
     }
     ,{
       logo:<RiCustomerService2Fill/>,
-      title: 'Founded With Passion',
-      discription: 'Started in 2015, FurniNest was born a passion for beautiful and functional living spaces'
+      title: 'Serving With Trust',
+      discription: 'We pride ourselves on excellent customer service, providing a seamless shopping experience from start.'
     }
   ]
 
   return (
-    <div  className='min-h-[50vh] just_col border'>
+    <div  className='min-h-[50vh] just_col gap-15 py-4'>
       <h2 className='title_text'>Our story</h2>
-      <div className='flex'>
+      <div className='flex gap-8'>
         {story.map((elem, idx) => (
-          <div key={idx} className='bg-amber-100'>
-            <div>{elem.logo}</div>
-            <h2>{elem.title}</h2>
-            <p>{elem.discription}</p>
+          <div key={idx} className='bg-[#ffffff] relative h-45 w-[22vw] just_col rounded-2xl px-4 gap-3 bg_sub '>
+            <div className='h-12 w-12 -top-6 just_col  absolute bg-[#4c9375] rounded-full text-neutral-100 '>{elem.logo}</div>
+            <h2 className='text-[16px] text-[#1a1a1a] font-medium '>{elem.title}</h2>
+            <p className='sub_text leading-6 '>{elem.discription}</p>
           </div>
         ))}
       </div>
