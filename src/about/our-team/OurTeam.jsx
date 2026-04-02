2d75821cbdf5ff033e8caadb7a4aad965babc84f
DragnelProjects <291502intekhab@gmail.com>
2026-04-02 19:46:12 +0530

    final commit

--- src/about/our-team/OurTeam.jsx
+++ src/about/our-team/OurTeam.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function OurTeam() {
   return (
-    <div className='min-h-screen'>OurTeam</div>
+    <div className='min-h-screen just_col border '>
+      <h2 className='title_text'>Meet Our Team</h2>
+      <p>Our passion for furniture and design is brought to life by our delitated seust of experts. Get to know the people behind FurniNest.</p>
+    </div>
   )
 }
 
