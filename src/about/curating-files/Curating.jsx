b3643f45628807b60e34dda834a0a44a31cd8db6
DragnelProjects <291502intekhab@gmail.com>
2026-04-02 08:32:23 +0530

    implemation complited

--- src/about/curating-files/Curating.jsx
+++ src/about/curating-files/Curating.jsx
@@ -4,7 +4,7 @@ import CuratingText from './CuratingText'
 
 function Curating() {
   return (
-    <div className='min-h-screen items_col py-10 gap-4 bg-[#ffffe9] border border-rose-500 '>
+    <div className='min-h-screen items_col py-10 gap-4 border '>
       <h2 className='title_text '>Curating for you</h2>
       <div className='w-full h-[30vw] flex items-center justify-center gap-8  '>
         <img src={CuratingImg} className='h-[90%] w-[30%] object-cover rounded-xl ' />
