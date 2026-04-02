e24d4d7c1ba7e58a0ddf4eb1c79314ce82873adf
DragnelProjects <291502intekhab@gmail.com>
2026-04-02 08:29:01 +0530

    last commit

--- src/about/choose-us/ChooseTop.jsx
+++ src/about/choose-us/ChooseTop.jsx
@@ -7,10 +7,10 @@ function ChooseTop(props) {
         <h2 className='title_text '>Furniture you can feel good about</h2>
         <div className='flex gap-6 '>
             {props.reasons.map((elem, idx) => (
-                <div key={idx} className='bg-[#ffffff] h-[16vw] w-[18vw] just_col px-3 gap-4 rounded-xl bg_sub '>
+                <div key={idx} className='bg-[#ffffff] h-[16vw] w-[18vw] just_col px-3 gap-2 rounded-xl bg_sub '>
                     <h1 className='text-[#699482] '>{elem.logo}</h1>
                     <h4 className='font-bold text-[#27292e] '>{elem.service}</h4>
-                    <p className='sub_text'>{elem.discrip}</p>
+                    <p className='discription'>{elem.discrip}</p>
                 </div>
             ))}
         </div>
