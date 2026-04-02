9e933ab63157eff606ff64c4c474dcab49dfcac6
DragnelProjects <291502intekhab@gmail.com>
2026-04-02 08:31:43 +0530

    in progress

--- src/about/curating-files/Curating.jsx
+++ src/about/curating-files/Curating.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import CuratingImg from '../about-images/curating.png'
+import CuratingText from './CuratingText'
 
 function Curating() {
   return (
-    <div className='min-h-screen'>Curating</div>
+    <div className='min-h-screen items_col py-10 gap-4 bg-[#ffffe9] border border-rose-500 '>
+      <h2 className='title_text '>Curating for you</h2>
+      <div className='w-full h-[30vw] flex items-center justify-center gap-8  '>
+        <img src={CuratingImg} className='h-[90%] w-[30%] object-cover rounded-xl ' />
+        <CuratingText/>
+      </div>
+    </div>
   )
 }
 
---
+++ src/about/curating-files/CuratingText.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function CuratingText() {
+  return (
+    <div className=' h-full w-[30vw] flex_col justify-center gap-4 px-4 '>
+        <h2 className='text-2xl font-semibold w-65 '>Elevate Your Space with Curated Selection</h2>
+        <p className='sub_text  text-justify '>Our team travels the globe to find unique, high-quality piecs that elevate any corner of your home, from classic staples to accent features.</p>
+        <button className='bg-[#196344c0] text-[#d7ffee] text-xs py-2 rounded-full w-[10vw] '>Discover More</button>
+
+    </div>
+  )
+}
+
+export default CuratingText
