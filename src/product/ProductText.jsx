9e6237510c76fe7f2af629782609034bda4fccc4
DragnelProjects <291502intekhab@gmail.com>
2026-04-03 10:24:57 +0530

    top text implemented

--- src/product/ProductText.jsx
+++ src/product/ProductText.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function ProductText() {
   return (
-    <div>ProductText</div>
+    <div className='min-h-[10vw] flex_col justify-center pl-4 py-8 gap-3'>
+        <h2 className='title_text'>Shop All Furniture</h2>
+        <p className='text-[12px] text-[#908f8f] tracking-wide w-110 '>Explore our wide range of modern and stylish furniture to elevate your living spaces.</p>
+    </div>
   )
 }
 
