afa5915010c537b2f6f9ed8be7ba8e977859f786
DragnelProjects <291502intekhab@gmail.com>
2026-04-04 17:14:16 +0530

    improved

--- src/product/Product.jsx
+++ src/product/Product.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
-import Section1 from '../section-one/Section1'
-import MainDisplay from './MainDisplay'
-import ProductText from './ProductText'
 import IntroSection from './IntroSection'
 
 function Product() {
   return (
     <div className='h-full w-full flex flex-col bg-[#ffffe8] scroll-smooth '>
       <IntroSection/>
-      <ProductText/>
-      <MainDisplay/>
+     
     </div>
   )
 }
