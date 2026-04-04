de328989b2ca591b7da03d58653e1f0680d62cf9
DragnelProjects <291502intekhab@gmail.com>
2026-04-04 09:18:01 +0530

    added for the product page

---
+++ src/product/IntroSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import IntroText from './product-images/IntroText'
+
+function IntroSection() {
+  return (
+    <div className='w-full h-[80%] flex flex-col items-center justify-center gap-6 mt-4 '>
+        <img src={}  className=' h-full w-full absolute object-center contrast-100 brightness-60 -z-10 ' />
+        <IntroText/>
+    </div>
+  )
+}
+
+export default IntroSection
