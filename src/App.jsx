71d22cd992bd8baa8319fe19f04fe7b8df48d694
DragnelProjects <291502intekhab@gmail.com>
2026-04-06 11:05:57 +0530

    improve routing

--- src/App.jsx
+++ src/App.jsx
@@ -10,26 +10,25 @@ import Section6 from './home/footer/Section6'
 import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
 import { products } from './javascript/product'
-import ProShow from './product/ProShow'
+import ProShow from './product/NewArrival'
+import NewArrival from './product/NewArrival'
 
   gsap.registerPlugin(useGSAP);
   
 
 function App() {
-
-
-
-
   return (
     <main className='h-full relative w-screen flex flex-col '>
       <Navbar />
       <Routes>
         <Route path='/' element={<Home/>}>
+          <Route index element={<SecThreeCards products={products}  /> } />
           <Route path=':id' element={<SecThreeCards products={products}  /> } />
         </Route>
         <Route path='/about' element={<About />}/>
         <Route path='/product' element={<Product/>}>
-            <Route path=':category' element={<ProShow/>} />
+            <Route index element={<NewArrival/>} />
+            <Route path=':category' element={<NewArrival/>} />
         </Route>
         <Route path='/contact' element={<Contact />} />
       </Routes>
