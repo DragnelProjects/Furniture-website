953e805607c1f37542a7245d2aeab4bd0e56cf9e
DragnelProjects <291502intekhab@gmail.com>
2026-04-06 11:17:50 +0530

    initial commit

---
+++ src/product/new-arrivals/NewArrival.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ArrivalText from './ArrivalText'
+
+function NewArrival() {
+  return (
+    <div className='min-h-screen items_col py-4 border'>
+      <ArrivalText/>
+    </div>
+  )
+}
+
+export default NewArrival
