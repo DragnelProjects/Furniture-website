76cd6d73c808703213a3be26225ba87eb65d7412
DragnelProjects <291502intekhab@gmail.com>
2026-04-06 11:17:31 +0530

    initial commit

---
+++ src/product/new-arrivals/BootomArrival.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BootomArrival() {
+  return (
+    <div>BootomArrival</div>
+  )
+}
+
+export default BootomArrival
---
+++ src/product/new-arrivals/TopArrival.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TopArrival() {
+  return (
+    <div>TopArrival</div>
+  )
+}
+
+export default TopArrival
