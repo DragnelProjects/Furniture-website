d809e04d747eccbac32ba42e123f1ac854d3e12f
DragnelProjects <291502intekhab@gmail.com>
2026-04-06 11:17:08 +0530

    implemented the arrival section text

---
+++ src/product/new-arrivals/ArrivalText.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ArrivalText() {
+  return (
+    <div className='just_col gap-3'>
+        <h2 className='title_text'>New Arrivals</h2>
+        <p className='sub_text w-140 text-center tracking-wide leading-6 '>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore similique modi temporibus amet itaque error earum dolor doloribus?</p>
+    </div>
+  )
+}
+
+export default ArrivalText
