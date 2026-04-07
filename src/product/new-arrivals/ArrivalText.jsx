d899607e415382abd52354acc932105c488be44b
DragnelProjects <291502intekhab@gmail.com>
2026-04-07 07:02:02 +0530

    implemented the text of arrival section

--- src/product/new-arrivals/ArrivalText.jsx
+++ src/product/new-arrivals/ArrivalText.jsx
@@ -4,7 +4,7 @@ function ArrivalText() {
   return (
     <div className='just_col gap-3'>
         <h2 className='title_text'>New Arrivals</h2>
-        <p className='sub_text w-140 text-center tracking-wide leading-6 '>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore similique modi temporibus amet itaque error earum dolor doloribus?</p>
+        <p className='sub_text w-140 text-center tracking-wide leading-6 '>Discover the latest design where comfort meets craftsmanship. Fall in love with furniture thata feels as good as it looks.</p>
     </div>
   )
 }
