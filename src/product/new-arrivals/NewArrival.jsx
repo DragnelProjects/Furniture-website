4bf4a48fbb2699eec3b07e14756560136c8131be
DragnelProjects <291502intekhab@gmail.com>
2026-04-07 14:53:19 +0530

    in progress

--- src/product/new-arrivals/NewArrival.jsx
+++ src/product/new-arrivals/NewArrival.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import ArrivalText from './ArrivalText'
 import TopArrival from './TopArrival'
 import BootomArrival from './BootomArrival'
+import { showcaseProduct } from '../../javascript/product'
 
 function NewArrival() {
   return (
-    <div className='min-h-screen items_col py-4 border'>
+    <div className='min-h-[50vw] w-full  items_col py-4 overflow-x-auto perspective-distant border'>
       <ArrivalText/>
-      <TopArrival/>
+      <TopArrival showcaseProduct={showcaseProduct} />
       <BootomArrival/>
     </div>
   )
--- src/product/new-arrivals/TopArrival.jsx
+++ src/product/new-arrivals/TopArrival.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
-function TopArrival() {
+
+function TopArrival(props) {
+
+  const { category } = useParams();
+  const active = category || "sofa2";
+
+  const filtered = props.showcaseProduct.filter((p) => p.category === active);
+  const newItems = filtered.filter((p)=> p.isNew === true  );
+
   return (
-    <div>TopArrival</div>
+    <div className='h-[26vw]  w-full  flex items-center  overflow-x-auto gap-6   border'>
+      {newItems.map((elem, idx) => (
+        <div key={idx} className='h-[22vw] w-[23vw] shrink-0 relative flex flex-col justify-center px-5 bg-gray-200'>
+           <div className='h-[60%] w-full just_col border '>
+                <img src={elem.image} alt={elem.name} className='h-full w-full object-contain ' />
+           </div>
+           <h4 className='sub_text'>{elem.name}</h4>
+           <h5 className='text-[14px] text-[#3f3e3e] font-medium '>{elem.price}</h5>
+        </div>
+      ))}
+      
+    </div>
   )
 }
 
