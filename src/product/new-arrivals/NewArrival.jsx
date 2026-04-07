60617003a644362838d58ff48551e04bf03b8137
DragnelProjects <291502intekhab@gmail.com>
2026-04-07 07:02:14 +0530

    initial commit

--- src/product/new-arrivals/NewArrival.jsx
+++ src/product/new-arrivals/NewArrival.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ArrivalText from './ArrivalText'
+import TopArrival from './TopArrival'
+import BootomArrival from './BootomArrival'
 
 function NewArrival() {
   return (
     <div className='min-h-screen items_col py-4 border'>
       <ArrivalText/>
+      <TopArrival/>
+      <BootomArrival/>
     </div>
   )
 }
