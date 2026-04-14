f516559b5a6d1ed99a668eef76f2da771ce731b7
DragnelProjects <291502intekhab@gmail.com>
2026-04-14 10:38:53 +0530

    added gsap to this section

--- src/section-one/OneMiddle.jsx
+++ src/section-one/OneMiddle.jsx
@@ -1,8 +1,27 @@
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react';
+import { SplitText } from 'gsap/SplitText'
 import React from 'react'
 
 function OneMiddle() {
+
+    useGSAP(() => {
+    const pargraphSplit = new SplitText('#subtitle', {type: 'lines'});
+    pargraphSplit.lines.forEach((line) => line.classList.add('text_gradient'))
+
+    gsap.from(pargraphSplit.lines,{
+      y:100,
+      opacity:0,
+      duration:2.5,
+      ease:'expo.out',
+      stagger: 0.4
+    })
+  })
+
+  
+
   return (
-    <div className='w-full h-[80%] flex flex-col items-center justify-center gap-6 mt-4 '>
+    <div id='subtitle' className='w-full h-[80%] flex flex-col items-center justify-center gap-6 mt-4 '>
         <h2 className='text-4xl text-[white] font-semibold w-150 text-center title '>Find The Perfect Furniture To Complete Your Home</h2>
         <div className=' flex flex-col gap-1 text-center  '>
             <p className='text-[#ababab] text-[12px] font-medium '>Discover furniture curated for modern living Shop, explore and design your perfect space</p>
