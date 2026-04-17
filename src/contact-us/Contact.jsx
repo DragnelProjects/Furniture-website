3a6198f023ba22b89f5618a47e64daafe76e5ee1
DragnelProjects <291502intekhab@gmail.com>
2026-04-17 08:59:32 +0530

    in progress

--- src/contact-us/Contact.jsx
+++ src/contact-us/Contact.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import ContactIntro from './ContactIntro'
+import FormMain from './FormMain'
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div className='h-full w-full flex flex-col bg-[#ffffe8] scroll-smooth '>
+      <ContactIntro/>
+      <FormMain/>
+    </div>
   )
 }
 
