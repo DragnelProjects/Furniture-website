3a3963e863071cc8912f2a4ac1d99973ee2ea2ce
DragnelProjects <291502intekhab@gmail.com>
2026-04-17 19:33:08 +0530

    form work is in progress

--- src/contact-us/ContactForm.jsx
+++ src/contact-us/ContactForm.jsx
@@ -6,12 +6,27 @@ function ContactForm() {
         <h2>Contact Form</h2>
         <h5>Fill this out so we can learn more about you and your needs.</h5>
         <form action="#" className='w-full h-[26vw] flex flex-col border'>
-            <input type="text" id='userName' />
-            <label htmlFor="userName">Name and Suranme</label>
-            <input type="text" id='userEmail' />
-            <label htmlFor="userEmail">Email Address</label>
-            <input type="text" id='userPhone' />
-            <label htmlFor="userPhone">Phone Number</label>
+            <div className=' h-[3.5vw] w-full relative '>
+              <input type="text" 
+                id='userName'
+                className='h-full w-full outline-0 border-b-2 border-gray-300 ' 
+              />
+              <span className='absolute left-2 top-1/5 '>Name and Surname</span>
+            </div>
+            <div className=' h-[3.5vw] w-full relative '>
+              <input type="text" 
+                id='userEmail' 
+                className='h-full w-full outline-0 border-b-2 border-gray-300 '
+                />
+            <span className='absolute left-2 top-1/5 ' >Email Address</span>
+            </div>
+            <div className=' h-[3.5vw] w-full relative '>
+              <input type="text" 
+                id='userPhone' 
+                className='h-full w-full outline-0 border-b-2 border-gray-300 '
+                />
+              <span className='absolute left-2 top-1/5 ' >Phone Number</span>
+            </div>
             <textarea  id="userMessage" className='border'>Write your message here</textarea>
         </form>
     </div>
