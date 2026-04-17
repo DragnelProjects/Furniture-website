cc423faec704169d9db928bef1b71f6a0ce64f4d
DragnelProjects <291502intekhab@gmail.com>
2026-04-17 08:12:56 +0530

    done some minor changes

--- src/section-one/Navbar.jsx
+++ src/section-one/Navbar.jsx
@@ -4,9 +4,9 @@ import {RiSearch2Line, RiHeartLine, RiLuggageCartLine } from '@remixicon/react'
 
 function Navbar() {
   return (
-    <div className=' flex w-full absolute top-0 items-center justify-between py-1 px-12 bg-[#0000000c] z-20 '>
-      <h2 className='text-3xl text-[#cccccc] font-light '>FurniNest</h2>
-      <div className=' flex text-[14px] text-[#e8e8e8] font-[250]  gap-12'>
+    <div className=' flex w-full absolute top-0 items-center justify-between py-1 px-12 z-20 '>
+      <h2 className='text-3xl text-[#ebebeb] font-light '>FurniNest</h2>
+      <div className=' flex text-[14px] text-[#ebebeb] font-[250]  gap-12'>
        <Link to='/' >Home</Link>
        <Link to='/about' className='active:scale-95' >About</Link>
        <Link to='/product' >Product</Link>
@@ -16,7 +16,7 @@ function Navbar() {
         <RiSearch2Line size={16} color='#d1d1d1' />
         <RiHeartLine size={16} color='#d1d1d1' />
         <RiLuggageCartLine size={16} color='#d1d1d1' />
-        <h5 className='text-[#d1d1d1] text-[14px] border-l border-l-[#858585] pl-2 '>Login</h5>
+        <h5 className='text-[#ebebeb] text-[14px] border-l border-l-[#858585] pl-2 '>Login</h5>
       </div>
     </div>
   )
