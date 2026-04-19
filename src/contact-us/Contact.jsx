512cf7309865413c4d0f3e91dd7ee81a582a73c7
DragnelProjects <291502intekhab@gmail.com>
2026-04-19 09:25:13 +0530

    implemented the form for contacting

--- src/contact-us/Contact.jsx
+++ src/contact-us/Contact.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ContactIntro from './ContactIntro'
 import FormMain from './FormMain'
 
+
 function Contact() {
   return (
     <div className='h-full w-full flex flex-col bg-[#ffffe8] scroll-smooth '>
