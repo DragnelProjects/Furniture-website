e7263eff07d0a5ea4ac8ec819007e81ab0d093bc
DragnelProjects <291502intekhab@gmail.com>
2026-04-20 09:05:43 +0530

    final commit

--- src/contact-us/Address.jsx
+++ src/contact-us/Address.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {RiFacebookLine, RiInstagramLine, RiPinterestLine,  RiYoutubeLine,  RiTwitterLine, RiBold } from '@remixicon/react'
 
 
 function Address() {
@@ -22,8 +23,14 @@ function Address() {
           <h4 className='text-[0.9rem] text-[#2a2828] font-medium '><span className='text-[0.8rem] text-[#505759]  font-medium '>Call:</span> +44(0)64396139649</h4>
           <h2 className='text-[0.9rem] text-[#2d3031] font-medium italic '>Monday - Friday: 9:00 AM to 6:00pm </h2>
       </div>
-      <div>
-        face
+      <div className='flex h-[3.5vw]'>
+        <ul className='flex gap-2'>
+          <li className='h-full w-[3.5vw]  bg-[#8f8f8f87] just_col rounded-full'><RiFacebookLine size={18} color='#f4f4f4' /></li>
+          <li className='h-full w-[3.5vw] bg-[#8f8f8f87]  just_col rounded-full'>< RiInstagramLine size={18} color='#f4f4f4'/></li>
+          <li className='h-full w-[3.5vw] bg-[#8f8f8f87]  just_col rounded-full'><RiPinterestLine size={18} color='#f4f4f4' /></li>
+          <li className='h-full w-[3.5vw] bg-[#8f8f8f87]  just_col rounded-full'>< RiYoutubeLine size={18} color='#f4f4f4' /></li>
+          <li className='h-full w-[3.5vw] bg-[#8f8f8f87]  just_col rounded-full'>< RiTwitterLine size={18} color='#f4f4f4'/></li>
+        </ul>
       </div>
     </div>
   )
