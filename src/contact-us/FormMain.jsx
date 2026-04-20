cfde20d6a393de060a0fd1ca20f10f8bce1edc87
DragnelProjects <291502intekhab@gmail.com>
2026-04-20 09:06:18 +0530

    in progress

--- src/contact-us/FormMain.jsx
+++ src/contact-us/FormMain.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import ContactForm from './ContactForm'
 import Address from './Address'
 
 function FormMain() {
+  const mainDisplay = useRef();
+  
+ 
+
   return (
-    <div className='h-[86vh] flex items-center justify-center  border '>
-        <ContactForm/>
+    <div ref={mainDisplay}  className='h-[86vh] flex items-center justify-center  border '>
+        <ContactForm  />
         <Address/>
     </div>
   )
